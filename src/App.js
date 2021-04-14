7a8c91eb62df55d93abf80ec988396fe29a84de9
Jose JABG. Antonio Belchior Gomes <josegomes@gac.local>
2021-04-14 19:52:30 -0300

    a react app to show expenses

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ExpenseListItems from "./components/Expenses/ExpensesListItems";
+import NewExpense from "./components/NewExpense/NewExpense";
 
-function App() {
+const App = () => {
+  const expences = [
+    {
+      id: "1",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 20),
+    },
+    {
+      id: "2",
+      title: "New TV",
+      amount: 799.99,
+      date: new Date(2021, 0, 15),
+    },
+    {
+      id: "3",
+      title: "Toilet Paper",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Hummmmm</h2>
+      <p>This is also visible!</p>
+      <NewExpense />
+      <ExpenseListItems items={expences} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Expenses/ExpensesListItems.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import ExpenseItem from "./ExpenseItem";
+import "./ExpenseListItems.css";
+import Card from "../UI/Card";
+
+const ExpenseListItems = (props) => {
+  const items = props.items.map((item) => {
+    return (
+      <ExpenseItem date={item.date} amount={item.amount} title={item.title} />
+    );
+  });
+
+  return <Card className="expense-list">{items}</Card>;
+};
+
+export default ExpenseListItems;
